a022ef00aa64fc63200c6dfc78a38623e5a2d2eb
zck0663 <1501045682@qq.com>
2019-03-26 15:25:12 +0800

    自定义标题栏

--- app/src/main/java/net/lzzy/cinemanager/activities/MainActivity.java
+++ app/src/main/java/net/lzzy/cinemanager/activities/MainActivity.java
@@ -2,20 +2,75 @@ package net.lzzy.cinemanager.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.SearchView;
+import android.widget.TextView;
 
 import net.lzzy.cinemanager.R;
 
 /**
  * @author Administrator
  */
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private LinearLayout layoutMenu;
+    private TextView tvTitle;
+    private SearchView search;
+    private LinearLayout layoutAddOrder;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         supportRequestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
+        setTitleMenu();
+    }
+
+    /**
+     * 标题栏
+     **/
+    private void setTitleMenu() {
+        layoutMenu = findViewById(R.id.bar_title_layout_menu);
+        layoutMenu.setVisibility(View.GONE);
+        findViewById(R.id.bar_title_img_menu).setOnClickListener(this);
+        findViewById(R.id.bar_title_img_menu).setOnClickListener(v -> {
+            int visible = layoutMenu.getVisibility() == View.VISIBLE ? View.GONE : View.VISIBLE;
+            layoutMenu.setVisibility(visible);
+        });
+        tvTitle = findViewById(R.id.bar_title_tv_title);
+        tvTitle.setText(R.string.bar_title_menu_orders);
+        search = findViewById(R.id.main_sv_search);
+        findViewById(R.id.bar_title_tv_add_cinema).setOnClickListener(this);
+        findViewById(R.id.bar_title_tv_view_cinema).setOnClickListener(this);
+        findViewById(R.id.bar_title_tv_add_order).setOnClickListener(this);
+        findViewById(R.id.bar_title_tv_view_order).setOnClickListener(this);
+        findViewById(R.id.bar_title_tv_exit).setOnClickListener(v -> {
+            System.exit(0);
+        });
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.bar_title_tv_add_cinema:
+                break;
+            case R.id.bar_title_tv_view_cinema:
+                finish();
+                break;
+            case R.id.bar_title_tv_add_order:
+                layoutAddOrder.setVisibility(View.VISIBLE);
+                layoutMenu.setVisibility(View.GONE);
+                break;
+            case R.id.bar_title_tv_view_order:
+                layoutMenu.setVisibility(View.GONE);
+                break;
+
+            default:
+                break;
+        }
     }
 }
